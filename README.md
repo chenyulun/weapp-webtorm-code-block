f7578c4be0b229519437660e50a3951d10a428e5
ex-chenyulun001 <ex-chenyulun001@pingan.com.cn>
2017-03-30 18:53:35 +0800

    weapp 0.15.152900

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # weapp-webtorm-code-block
 微信小程序webstorm代码块插件
+## 项目介绍
+为了方便开发者在webstorm上开发，抽取微信web工具源码里面的editor里面的js代码模块，重新组装成webstorm版本的代码块插件
+
+## 使用方法
+直接使用方法：
+  
+mac系统： 把dist/wx.xml文件放入"/Users/你的用户名/Library/Preferences/WebStorm版本/templates"文件夹
+window系统：c:\users\用户名\WebStorm版本\config\templates文件夹  
+如果文件夹不存在请自行建立；
+
+重新构建方法:  
+
+把微信web工具里面的安装包里面的
+```
+/Applications/wechatwebdevtools.app/Contents/Resources/app.nw/app/dist/editor/index.min.js
+```
+放到项目的src/lib里面；
+然后执行
+
+```
+$ npm run build
+
+```
---
+++ index.js
@@ -0,0 +1,117 @@
+/**
+ * Created by chenyulun on 2017/3/30.
+ */
+let fs = require('fs');
+let path = require('path');
+let editorPath = "src/lib/index.min.js";
+
+// 读取文件编辑器文件
+function readFile(fileName) {
+	return new Promise(function (resolve, reject) {
+		fs.readFile(fileName, function (error, data) {
+			if (error) reject(error);
+			resolve(data);
+		});
+	});
+}
+
+// 写入webstorm wx.xml
+function writeFile(fileName, w_data) {
+	return new Promise(function (resolve, reject) {
+		fs.writeFile(fileName, w_data, {flag: 'w'}, function (err) {
+			if (err) {
+				consoel.log(err);
+				reject(err)
+			} else {
+				resolve("成功写入");
+			}
+			;
+		});
+	})
+}
+
+//代码块输入后输入属性
+function getVariableList(variableObject) {
+	let variableList = ``;
+	for ([key, value] of Object.entries(variableObject)) {
+		variableList += `      <variable name="${key}" expression="" defaultValue="${"&quot;" + value + "&quot;"}" alwaysStopAt="true" />
+`;
+	}
+	return variableList;
+
+
+}
+
+//每个API方法生成代码块xml
+function temple(options) {
+	let Variable = getVariableList(options.variableObject);
+
+	return `  <template name="${options.name}" value="${options.value}" description="${options.documentation}" toReformat="true" toShortenFQNames="true">
+${Variable}      <context>
+        <option name="JS_STATEMENT" value="true" />
+      </context>
+  </template>
+`;
+}
+
+//处理weixin原始代码块方法
+function transformToLineTemple(key, value) {
+	let i = 0;
+	let variableObject = {};
+	value.insertText = "wx." + value.insertText
+			.replace(/\n/g, "&#10;").replace(/\\'/g, "")
+			.replace(/{{({[\S]*?})}}|{{(\/\/\s[\S]*?)}}|{{([\S]*?)}}/g, function (word, $1, $2, $3) {
+				let value = $1 || $2 || $3 || "";
+				i++;
+				variableObject["String" + i] = value;
+				return "$String" + i + "$"
+			})
+			.replace(/"/g, "'");
+	let name = "wx" + key[0].toUpperCase() + key.substring(1, key.length);
+	return temple({
+		variableObject: variableObject,
+		name: name,
+		value: value.insertText,
+		documentation: value.documentation,
+	})
+
+
+}
+
+async function getFile() {
+
+	//直接执行内部有一个属性指向xxx,为了不报错，写兼容属性
+	let monaco = {
+		languages: {
+			CompletionItemKind: {
+				Function: function () {
+				}
+			}
+		}
+	};
+
+	//读取原始文件
+	let File = await readFile(path.resolve(__dirname, editorPath));
+	//提前想要的API，直接执行41模块；以后官方更新后可能会变化
+	eval(File.toString().replace('return t.m=e,t.c=n,t.p="",t(0)', 'return t.m=e,t.c=n,t.p="",t(41)'));
+
+	//开始生成webstorm用代码块xml文件
+	let lineTempFile =
+		`<templateSet group="wx">
+`;
+	for ([key, value] of Object.entries(module.exports.api)) {
+		lineTempFile += transformToLineTemple(key, value);
+	}
+	for ([key, value] of Object.entries(module.exports.mina)) {
+		lineTempFile += transformToLineTemple(key, value);
+	}
+	lineTempFile += `</templateSet>`;
+	let backString = await writeFile(path.resolve(__dirname, "dist/wx.xml"), lineTempFile);
+	console.log(backString,",请查看dist目录的wx.xml");
+	//return File;
+}
+getFile();
+
+
+
+
